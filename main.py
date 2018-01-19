1e59f010cddf2eaac2ba40411d96408dcd332cdd
Keith Cestaro <34603915+KeithCes@users.noreply.github.com>
2018-01-19 02:38:31 -0500

    beginings of hand evaluation

--- main.py
+++ main.py
@@ -38,35 +38,149 @@ def deal(deck):
     translateCard(playerHand, 1)
     translateCard(opponentHand, 0)
     translateCard(opponentHand, 1)
-    displayCards()
-    input()
+#    displayCards()
+#    input()
 
 
 def flop(deck):
     translateCard(communityCards, 0)
     translateCard(communityCards, 1)
     translateCard(communityCards, 2)
-    displayCards()
-    input()
+#    displayCards()
+#    input()
 
 
 def turn(deck):
     translateCard(communityCards, 3)
-    displayCards()
-    input()
+#    displayCards()
+#    input()
 
 
 def river(deck):
     translateCard(communityCards, 4)
-    displayCards()
-    input()
+#    displayCards()
+#    input()
+
+
+def checkHand(hand):
+    nums = []
+    suits = []
+    i = 0
+    while i < 7:
+        nums.append(str(hand[i])[0])
+        suits.append(str(hand[i])[1])
+        i += 1
+    checkPair(nums)
+    checkFlush(suits)
+    checkStraight(nums)
+
+
+def checkPair(lst):
+    pairs = []
+    threeKind = []
+    fourKind = []
+    fullHouse = False
+    twoPair = False
+    tempLst = lst
+    tempLst.sort()
+    tempLst.append("55")
+    tempLst.append("56")
+    tempLst.append("57")
+    tempLst.append("58")
+    i = 0
+    while i < 7:
+        if (tempLst[i] == tempLst[i + 1] and tempLst[i] == tempLst[i + 2] and tempLst[i] == tempLst[i + 3]):
+            fourKind.append(tempLst[i])
+            tempLst.remove(tempLst[i])
+            tempLst.remove(tempLst[i + 1])
+            tempLst.remove(tempLst[i + 2])
+        elif tempLst[i] == tempLst[i + 1] and tempLst[i] == tempLst[i + 2]:
+            threeKind.append(tempLst[i])
+            tempLst.remove(tempLst[i])
+            tempLst.remove(tempLst[i + 1])
+        elif tempLst[i] == tempLst[i + 1]:
+            pairs.append(tempLst[i])
+            tempLst.remove(tempLst[i])
+        i += 1
+        if len(pairs) > 2:
+            pairs.sort()
+            pairs.remove(pairs[0])
+        elif len(pairs) > 0 and len(threeKind) > 0:
+            fullHouse = True
+        elif len(pairs) > 1:
+            twoPair = True
+    print("Pairs: " + str(pairs))
+    print("Three Piece: " + str(threeKind))
+    print("Four Piece: " + str(fourKind))
+    print("Full House: " + str(fullHouse))
+    print("Two Pair: " + str(twoPair))
+
+
+def checkFlush(lst):
+    flush = False
+    amountS = 0
+    amountH = 0
+    amountC = 0
+    amountD = 0
+    i = 0
+    while i < 7:
+        if lst[i] == "s":
+            amountS += 1
+        elif lst[i] == "h":
+            amountH += 1
+        elif lst[i] == "c":
+            amountC += 1
+        elif lst[i] == "d":
+            amountD += 1
+        i += 1
+    if amountS == 5 or amountH == 5 or amountC == 5 or amountD == 5 :
+        flush = True
+    print("Flush: " + str(flush))
+
+
+def checkStraight(lst):
+    straight = False
+    q = 0
+    while q < 7:
+        if lst[q] == "J":
+            lst[q] = "11"
+        elif lst[q] == "Q":
+            lst[q] = "12"
+        elif lst[q] == "K":
+            lst[q] = "13"
+        elif lst[q] == "A":
+            lst[q] = "14"
+        q += 1
+    lst = sorted(lst, key = int)
+    straightCount = 1
+    i = 0
+    while i < 6 and straightCount < 5:
+        if int(lst[i]) + 1 == int(lst[i + 1]):
+            straightCount += 1
+        else:
+            straightCount = 1
+        i += 1
+    if straightCount >= 5:
+        straight = True
+    while q < 7:
+        if lst[q] == "11":
+            lst[q] = "J"
+        elif lst[q] == "12":
+            lst[q] = "Q"
+        elif lst[q] == "13":
+            lst[q] = "K"
+        elif lst[q] == "14":
+            lst[q] = "A"
+    print("Straight: " + str(straight))
 
 
 def reveal(deck):
     print("Your Hand: " + str(" ".join(playerHand)))
-    print("Your Hand: " + str(" ".join(opponentHand)))
+    print("Opponent's Hand: " + str(" ".join(opponentHand)))
     print("Community Cards: " + str(" ".join(communityCards)))
-    input()
+    playerHand.extend(communityCards)
+    opponentHand.extend(communityCards)
+    checkHand(playerHand)
 
 
 def translateCard(loc, num):
